12c631e5ee133f0fe431e67de45913ab408ddb06
Matthew Myrick <matthewmyrick2@gmail.com>
2025-07-16 13:22:44 -0400

    boilerplate

---
+++ harbor-mobile/harbor-mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  harbor-mobile
+//
+//  Created by Matthew.Myrick@anheuser-busch.com on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ harbor-mobile/harbor-mobile/harbor_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  harbor_mobileApp.swift
+//  harbor-mobile
+//
+//  Created by Matthew.Myrick@anheuser-busch.com on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct harbor_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
